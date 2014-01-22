b6e5beae40beeac978b117e95bd8657d86171be9
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:50:48 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/glut/build-mingw32.sh
+++ csrc/glut/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc $CFLAGS src/*.c -D FREEGLUT_EXPORTS -I. -Iinclude -shared -o ../../bin/mingw32/glut.dll \
+gcc -O2 -s -static-libgcc src/*.c -D FREEGLUT_EXPORTS -I. -Iinclude -shared -o ../../bin/mingw32/glut.dll \
 	-lopengl32 -lgdi32 -lwinmm
