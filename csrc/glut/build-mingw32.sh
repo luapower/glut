8e35b1d617eb7f9b579b062fe1cdca3ec30b87cb
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-02-01 00:54:53 +0200

    bugfix in build script

--- csrc/glut/build-mingw32.sh
+++ csrc/glut/build-mingw32.sh
@@ -1,2 +1,2 @@
 gcc -O2 -s -static-libgcc src/*.c -D FREEGLUT_EXPORTS -I. -Iinclude -shared -o ../../bin/mingw32/glut.dll \
-    -lopengl32 -lgdi32 -lwinmm
+	-DWINVER=0x0500 -lopengl32 -lgdi32 -lwinmm
